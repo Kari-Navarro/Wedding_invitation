39d0d75d56031d2ab4cc2cfb5bbd4f677cd6127d
Karina Navarro <karinavarro.b@gmail.com>
2024-06-04 19:02:28 -0600

    se comento el formulario para ver si se puede manejar el error

--- wedding_invitation/src/confirm.tsx
+++ wedding_invitation/src/confirm.tsx
@@ -5,7 +5,7 @@ const Assistance= () => {
   return (
   <section className="section-confirmacion">
     <h2 className="titulo-confirmacion">CONFIRMA TU ASISTENCIA</h2>
-    <iframe src="https://docs.google.com/forms/d/e/1FAIpQLSdv4BAdsMBy2d4t7AJpHaXwOs7wvg6TY2_O02PoAylNZKcW3A/viewform?embedded=true" width="340" height="900" frameborder="0" marginheight="0" marginwidth="0">Cargando…</iframe>
+    {/*<iframe src="https://docs.google.com/forms/d/e/1FAIpQLSdv4BAdsMBy2d4t7AJpHaXwOs7wvg6TY2_O02PoAylNZKcW3A/viewform?embedded=true" width="340" height="900" frameborder="0" marginheight="0" marginwidth="0">Cargando…</iframe>*/}
     </section>
   );
 };
