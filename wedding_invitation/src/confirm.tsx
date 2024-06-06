f4ee078e5d5cf6eee743c2b5578dc829b2ed0c4e
Karina Navarro <karinavarro.b@gmail.com>
2024-06-06 12:19:59 -0600

    se agregó el formulario formcade

--- wedding_invitation/src/confirm.tsx
+++ wedding_invitation/src/confirm.tsx
@@ -1,11 +1,16 @@
-
-
+import FormfacadeEmbed from "@formfacade/embed-react";
 
 const Assistance= () => {
   return (
   <section className="section-confirmacion">
     <h2 className="titulo-confirmacion">CONFIRMA TU ASISTENCIA</h2>
-    {/*<iframe src="https://docs.google.com/forms/d/e/1FAIpQLSdv4BAdsMBy2d4t7AJpHaXwOs7wvg6TY2_O02PoAylNZKcW3A/viewform?embedded=true" width="340" height="900" frameborder="0" marginheight="0" marginwidth="0">Cargando…</iframe>*/}
+    <FormfacadeEmbed
+
+formFacadeURL="https://formfacade.com/include/115105755408056572524/form/1FAIpQLSdv4BAdsMBy2d4t7AJpHaXwOs7wvg6TY2_O02PoAylNZKcW3A/classic.js/?div=ff-compose"
+
+onSubmitForm={() => console.log('Form submitted')}
+
+/>
     </section>
   );
 };
